a57376177cece5de6b13cf5e0f314ebd07fcd911
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-03 21:42:41 +0000

    Step 17 Complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,31 @@ require_relative "./bike.rb"
 
 class DockingStation
 
-  def release_bike
-    Bike.new
+  DEFAULT_CAPACITY = 20
+
+  def initialize(given_capacity = DEFAULT_CAPACITY)
+    @bikes = []
+    @capacity = given_capacity
   end
 
   def dock(bike)
-    @bike = bike
-
+    fail "There are already max bikes docked" if full?
+    @bikes << bike
   end
 
+  def release_bike
+    fail "No bikes available" if empty?
+    @bikes.pop
+  end
 
+  private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.count >= @capacity
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,28 +1,41 @@
 require "docking_station"
 
 describe DockingStation do
-  # it "responds to release_bike method" do
-  #   expect(subject).to respond_to (:release_bike)
-  # end
 
-  it { is_expected.to respond_to (:release_bike)}
+  describe "#Release bike" do
 
-  it "Releases a new bike upon .release_bike" do
-    bike = subject.release_bike
-    expect(bike.working?).to eq true
-  end
+    it { is_expected.to respond_to (:release_bike) }
 
-  it { is_expected.to respond_to(:dock).with(1).argument}
+    it "Releases a new bike upon .release_bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-  it "accepts an bike in the dock" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-
-
+    it "Will fail with no bikes" do
+      expect{ subject.release_bike }.to raise_error "No bikes available"
+    end
 
   end
 
+  describe "#Dock bike" do
 
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
+    it "accepts an bike in the dock" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq [bike]
+    end
+
+  end
+
+  describe "#Fail for bike above capacity" do
+
+    it "Will fail when capacity exceeded" do
+      DockingStation::DEFAULT_CAPACITY.times{ subject.dock Bike.new }
+      expect{ subject.dock Bike.new }.to raise_error "There are already max bikes docked"
+    end
+
+  end
 
 end
